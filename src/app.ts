fa18a183e4b90dfbb818b12c3d8c42e5e128ec86
sumit majumdar <sumitmajumdar16@gmail.com>
2024-06-10 23:19:46 +0530

    Task 25: Typescript showdown

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import todosRoutes from "./routes/todos";
+import todosRoutes from "./src/routes/todos";
 import bodyParser from "body-parser";
 const app = express();
 app.use(bodyParser.json());
